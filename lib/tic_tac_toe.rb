56a3f713030636a14936186a7b81160f9883b968
Sarah Paz <sarah.paz@gmail.com>
2018-12-07 15:13:16 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,50 @@
+class TicTacToe
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    input = input.to_i - 1
+  end
+
+  def move(index, player = "X")
+    @board[index] = player
+  end
+
+  def position_taken?(index)
+    if @board[index] == " " || @board[index] == ""
+      false
+    else @board[index] == "X" || @board[index] == "O"
+      true
+    end
+  end
+
+  def valid_move?(index)
+    if !position_taken?(index) && @board[index].between?(0,8)
+      true
+    else
+      false
+    end
+  end
+
+end
